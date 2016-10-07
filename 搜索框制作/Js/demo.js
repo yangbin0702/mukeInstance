2334234ce1b6f528c8d30f25d1cf0098e2233d9d
yangbib0702 <yangbin5914@sina.com>
2016-10-07 22:55:54 +0800

    first commit

--- 搜索框制作/Js/demo.js
+++ 搜索框制作/Js/demo.js
@@ -1,3 +1,42 @@
 /**
  * Created by Administrator on 2016/9/15.
  */
+var getDOM = function(id){
+    return document.getElementById(id);
+}
+
+var addEvent = function(id,event,fn){
+    var el = getDOM(id)||document;
+    if(el.addEventListener){
+        el.addEventListener(event,fn,false);
+    }else if(el.attachEvent){
+        el.attachEvent('on'+event,fn);
+    }
+}
+
+var getElementLeft = function(element){
+    var actualLeft = element.offsetLeft;
+    var current = element.offsetParent;
+    while(current != null){
+        actualLeft += current.offsetLeft;
+        current = current.offsetParent;
+    }
+    return actualLeft;
+}
+
+var getElementTop = function(element){
+    var actualTop = element.offsetTop;
+    var current = element.offsetParent;
+    while(current != null){
+        actualTop += current.offsetTop;
+        current = current.offsetParent;
+    }
+    return actualTop;
+}
+///////
+addEvent('search_input','keyup',function(){
+    getDOM('search-suggest').style.top = getElementTop(getDOM('search-form'))+38+'px';
+    getDOM('search-suggest').style.left = getElementLeft(getDOM('search-form'))+'px';
+    getDOM('search=suggest').style.position = 'absolute';
+    getDOM('search-suggest').style.display = 'block';
+})
